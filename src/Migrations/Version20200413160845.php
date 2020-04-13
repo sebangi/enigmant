d2ec794d8a149a547af59c52282e3b476b65473a
sebangi <sebastien_angibaud@yahoo.Fr>
2020-04-13 18:10:04 +0200

    Migration

---
+++ src/Migrations/Version20200413160845.php
@@ -0,0 +1,35 @@
+<?php
+
+declare(strict_types=1);
+
+namespace DoctrineMigrations;
+
+use Doctrine\DBAL\Schema\Schema;
+use Doctrine\Migrations\AbstractMigration;
+
+/**
+ * Auto-generated Migration: Please modify to your needs!
+ */
+final class Version20200413160845 extends AbstractMigration
+{
+    public function getDescription() : string
+    {
+        return '';
+    }
+
+    public function up(Schema $schema) : void
+    {
+        // this up() migration is auto-generated, please modify it to your needs
+        $this->abortIf($this->connection->getDatabasePlatform()->getName() !== 'mysql', 'Migration can only be executed safely on \'mysql\'.');
+
+        $this->addSql('ALTER TABLE jeu_en_chene CHANGE difficulte_observation difficulte_observation INT DEFAULT 5, CHANGE difficulte_raisonnement difficulte_raisonnement INT DEFAULT 5, CHANGE prix prix INT DEFAULT 1 NOT NULL');
+    }
+
+    public function down(Schema $schema) : void
+    {
+        // this down() migration is auto-generated, please modify it to your needs
+        $this->abortIf($this->connection->getDatabasePlatform()->getName() !== 'mysql', 'Migration can only be executed safely on \'mysql\'.');
+
+        $this->addSql('ALTER TABLE jeu_en_chene CHANGE difficulte_observation difficulte_observation INT DEFAULT NULL, CHANGE difficulte_raisonnement difficulte_raisonnement INT DEFAULT NULL, CHANGE prix prix INT NOT NULL');
+    }
+}
